783062bc8dbf005338ae6853dcee20b1e69bc0c4
Nimesh Ghelani <nimesh.g@directi.com>
2014-06-26 02:51:35 +0000

    Formatting inbuilt

--- getFootballScore.py
+++ getFootballScore.py
@@ -10,21 +10,32 @@ def getScoreString():
     content = urllib2.urlopen(url).read()
     html = lxml.html.fromstring(content)
 
-    matches = html.xpath('//div[contains(@class,"mEl")]//dl')
-    ret = []
+    matches = html.xpath('//div[contains(@class, "sdHpItem")]/div[contains(@class,"mEl")]//dl[dd]')
+    parsedList = []
     for match in matches:
         indiData = match.xpath('.//dd')
         parsedData = []
         for field in indiData:
             parsedData.append(''.join(map(str, field.xpath('.//text()'))))
         for i,x in enumerate(parsedData):
+            parsedData[i] = x.strip()
             if x == 'v':
                 parsedData[i] = '  v  '
-                
-        out = '\t'.join(parsedData).strip()
-        if len(out) > 0:
-            ret.append(out)
-    return '\n'.join(ret)
+        if len(''.join(parsedData)) == 0:
+            continue
+        if parsedData[0] == '':
+            parsedData = parsedData[1:]
+        parsedList.append(parsedData)
+
+    maxLen = reduce(max, map(len, parsedList))
+    for i,x in enumerate(parsedList):
+        parsedList[i] = ['']*(maxLen-len(x)) + x
+
+    maxColLen = [0]*maxLen
+    for i in xrange(maxLen):
+        maxColLen[i] = reduce(max, map(len, [x[i] for x in parsedList]))
+    parsedList = [[x[i].ljust(maxColLen[i]+2) for i in range(maxLen)] for x in parsedList]
+    return '\n'.join(map('\t'.join,parsedList))+'\n'
 
 class getFootballScore(webapp2.RequestHandler):
     def get(self):
@@ -36,4 +47,3 @@ application = webapp2.WSGIApplication([
         ('/', getFootballScore),
         ], debug=False)
 
-print getScoreString()
